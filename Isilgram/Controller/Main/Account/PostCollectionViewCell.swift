7aa20bfdcde40815e751b990da3d8f38be32fe0f
Jordi <jordi.diaz04@hotmail.com>
2019-12-09 12:34:22 -0800

    account view

--- Isilgram/Controller/Main/Account/PostCollectionViewCell.swift
+++ Isilgram/Controller/Main/Account/PostCollectionViewCell.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class PostCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var ivPost: CSMImageView!
     
     
+    //MARK: Variables and Components
+    var objPost: PostBE! {
+        didSet {
+            self.loadImages()
+        }
+    }
     
+    
+    //MARK: Created Functions
+    func loadImages() {
+        let stgPost: StorageReference! = Storage.storage().reference().child(objPost.author!)
+        let refPhoto = stgPost.child(objPost.pictures!.first!)
+        ivPost.sd_setImage(with: refPhoto, placeholderImage: UIImage(named: "img_default"))
+    }
 }
